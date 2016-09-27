013d67e0cf9f3135b85f084e08f718302bba41f9
FirstKlaas <klaas.nebuhr@gmail.com>
2016-09-27 21:26:22 +0200

    First REST api base on sqlite3

---
+++ sporthub/node_modules/mqtt/lib/connect/ws.js
@@ -0,0 +1,89 @@
+'use strict'
+
+var websocket = require('websocket-stream')
+var _URL = require('url')
+var wssProperties = [
+  'rejectUnauthorized',
+  'ca',
+  'cert',
+  'key',
+  'pfx',
+  'passphrase'
+]
+
+function buildBuilder (client, opts) {
+  var wsOpt = {
+    protocol: 'mqtt'
+  }
+  var host = opts.hostname || 'localhost'
+  var port = String(opts.port || 80)
+  var path = opts.path || '/'
+  var url = opts.protocol + '://' + host + ':' + port + path
+
+  if ((opts.protocolId === 'MQIsdp') && (opts.protocolVersion === 3)) {
+    wsOpt.protocol = 'mqttv3.1'
+  }
+
+  if (opts.protocol === 'wss') {
+    wssProperties.forEach(function (prop) {
+      if (opts.hasOwnProperty(prop)) {
+        wsOpt[prop] = opts[prop]
+      }
+    })
+  }
+
+  return websocket(url, wsOpt)
+}
+
+function buildBuilderBrowser (mqttClient, opts) {
+  var url
+  var parsed
+
+  // for Web Workers! P.S: typeof(document) !== undefined may be becoming the faster one these days.
+  if (typeof (document) !== 'undefined') {
+    parsed = _URL.parse(document.URL)
+  } else {
+    throw new Error('Could not determine host. Specify host manually.')
+  }
+
+  if (!opts.protocol) {
+    if (parsed.protocol === 'https:') {
+      opts.protocol = 'wss'
+    } else {
+      opts.protocol = 'ws'
+    }
+  }
+
+  if (!opts.hostname) {
+    opts.hostname = opts.host
+  }
+
+  if (!opts.hostname) {
+    opts.hostname = parsed.hostname
+    if (!opts.port) {
+      opts.port = parsed.port
+    }
+  }
+
+  if (!opts.port) {
+    if (opts.protocol === 'wss') {
+      opts.port = 443
+    } else {
+      opts.port = 80
+    }
+  }
+
+  if (!opts.path) {
+    opts.path = '/'
+  }
+
+  url = opts.protocol + '://' + opts.hostname + ':' + opts.port + opts.path
+
+  return websocket(url, 'mqttv3.1')
+}
+
+if (process.title !== 'browser') {
+  module.exports = buildBuilder
+} else {
+  module.exports = buildBuilderBrowser
+}
--- sporthub/server.js
+++ sporthub/server.js
@@ -1,15 +1,19 @@
-var express = require('express')
-,   app = express()
-,   server = require('http').createServer(app)
-,   io = require('socket.io').listen(server)
-,   conf = require('./config.json');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const conf = require('./config.json');
+const sqlite3 = require('sqlite3').verbose();
+const mqtt = require('mqtt');
 
-// Webserver
+// webserver
 // auf den Port x schalten
 server.listen(conf.port);
 
-app.use(express.static(conf.static_dir));
+var db = new sqlite3.Database(conf.database);
 
+// Statische Files aus diesem Pfad ausliefern
+app.use(express.static(conf.static_dir));
 
 // wenn der Pfad / aufgerufen wird
 app.get('/', function (req, res) {
@@ -17,13 +21,29 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/user', function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    db.all("SELECT * FROM user", function (err, rows) {
+        res.json({ "users" : rows });
+    });
+});
+
+app.get('/user/:id', function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    db.get("SELECT * FROM user WHERE id=?", [req.params.id], function (err, row) {
+        res.json({ "user" : row });
+    });
+});
+
 // Websocket
 io.sockets.on('connection', function (socket) {
 	// der Client ist verbunden
+	console.log('Connected');
 	socket.emit('chat', { zeit: new Date(), text: 'Du bist nun mit dem Server verbunden!' });
 	// wenn ein Benutzer einen Text senden
 	socket.on('chat', function (data) {
 		// so wird dieser Text an alle anderen Benutzer gesendet
+        console.log('Message: ' + data.text);
 		io.sockets.emit('chat', { zeit: new Date(), name: data.name || 'Anonym', text: data.text });
 	});
 });

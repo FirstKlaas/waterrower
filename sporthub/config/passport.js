fa0c2359ab9f95d8b18b56fedf12f8f010be3ed5
FirstKlaas <klaas.nebuhr@gmail.com>
2017-06-25 21:59:42 +0200

    Added local authentification based on passport

---
+++ sporthub/config/passport.js
@@ -0,0 +1,85 @@
+var LocalStrategy   = require('passport-local').Strategy;
+
+// expose this function to our app using module.exports
+module.exports = function(passport,db) {
+
+    // =========================================================================
+    // passport session setup ==================================================
+    // =========================================================================
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
+
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser((id, done) => {
+        db.getUser(id)
+        	.then(user => done(null,user))
+        	.catch(err => done(err));
+    });
+
+    // =========================================================================
+    // LOCAL SIGNUP ============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+    // by default, if there was no name, it would just be called 'local'
+
+    passport.use('local-signup', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'login',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, login, password, done) {
+
+        // asynchronous
+        // User.findOne wont fire unless data is sent back
+        process.nextTick(function() {
+
+	        db.getUserByLogin(login)
+	        	.then(user => {
+	        		if (user) {
+	        			done(null, false, req.flash('signupMessage', 'That account name is already taken.'));
+	        		} else {
+	        			// Nun einen neuen User anlegen
+	        			db.addNewUser(login,password)
+	        				.then(userid => {
+        						db.getUser(userid)
+        							.then(user => done(null, user))
+        							.catch(err => done(err));
+	        				})
+	        				.catch(err => done(err));
+	        		}
+	        	})
+	        	.catch(err => done(err));
+    	});
+	}));
+
+	// =========================================================================
+    // LOCAL LOGIN =============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+    // by default, if there was no name, it would just be called 'local'
+
+    passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'login',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, login, password, done) {
+    	db.validatePassword(login,password)
+    	.then(user => {
+    		if (!user) {
+    			done(null, false, req.flash('loginMessage', 'Falsche Anmeldedaten'));
+    		} else {
+    			done(null,user);
+    		}
+
+    	})
+    	.catch(err => done(err));
+    }));
+};

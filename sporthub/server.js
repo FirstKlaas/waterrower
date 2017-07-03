7ab10416190c27583c3be9a45378d35661950d98
unknown <nebuhr@cde000554.encowayhb.lokal>
2017-07-03 09:09:11 +0200

    No Session Filestore

--- sporthub/server.js
+++ sporthub/server.js
@@ -58,7 +58,7 @@ let session_config = {
 app.use(session({
     key: session_config.cookie,
     secret: session_config.secret,
-    store: session_config.store,resave: true,
+    resave: true,
     saveUninitialized: true 
 })); // session secret
 
